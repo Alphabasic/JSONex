61c18742ca99a32c989dfb95a5ffb34207debd50
unknown <samuelh2t@PTPZ001604.ptfc.local>
2014-10-24 15:18:39 -0700

    comment out db info

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 function openConn(){
     $server = 'PTPSV50137';
-    $options = array( "Database" => "EmpDir", "UID"=>"ITSDrawingIndex", "PWD"=>"654dwgfiles.A");
+    $options = array( "Database" => "EmpDir", "UID"=>"u_no", "PWD"=>"bro");
     $conn = sqlsrv_connect($server, $options);
     if ($conn === false) die("<pre>".print_r(sqlsrv_errors(), true));
     return $conn;}
